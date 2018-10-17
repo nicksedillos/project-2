6988dce20a75b4280386936818567b82dfad5905
Nick Sedillos <nick.sedillos@gmail.com>
2018-10-16 20:59:50 -0500

    .

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,7 +14,8 @@ module.exports = function(app) {
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample) {
+      dbExample
+          ) {
       res.render("example", {
         example: dbExample
       });
@@ -24,5 +25,6 @@ module.exports = function(app) {
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
+  
   });
 };
