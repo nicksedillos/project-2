bb5364f1421d090a08d3a8a94144170e33461194
ahend <anh0238@gmail.com>
2018-10-20 13:51:35 -0500

    inital react attempt

--- react/hide-and-seek/src/App.js
+++ react/hide-and-seek/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gameboardImage from '#'; //image path placeholder
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
+          <h1>
+            Hide-and-Seek
+          </h1>
+        </header>
+
+        <div className="Game-image">
+          <img src={gameboardImage} className="gameboard" alt="gameboard" />
+        </div>
+
+        <div className="scoreboard"></div>
+
+        <div className="chatbox"></div>
+
+        <div className="controls"></div>
+
+        <div className="App-footer">
+        <a
+            className="Repo-link"
+            href="https://github.com/nicksedillos/project-2"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            View the code on Github
           </a>
-        </header>
+        </div>
+
       </div>
     );
   }
