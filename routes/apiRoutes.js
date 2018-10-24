79a923237652d020ad315ef9f8ff99e11913fbd4
ahend <anh0238@gmail.com>
2018-10-23 21:31:28 -0500

    api routes created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,41 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+module.exports = function(game) {
+
+  // Post Player Info to database
+  db.post("api/player", function(req, res) {
+    db.Player.create(req.body).then(function(dbPlayer) {
+      res.json(dbPlayer);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Display current players
+  db.get("api/player", function(req, res) {
+    db.AllPlayers.findAll({ name }).then(function(dbAllPlayers) {
+      res.json(dbAllPlayers);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
+  // Check game state
+  db.get("api/gamestate", function(req, res) {
+    db.GameState.findAll({}).then(function(dbGameState) {
+      res.json(dbGameState);
     });
   });
+
+  // Get chat log
+  db.get("api/chat", function(req, res) {
+    db.Chat.findAll({}).then(function(allChats) {
+      res.json(allChats);
+    });
+  });
+
+  // Post last move
+  db.post("/api/position", function(req, res) {
+    db.Position.create(req.body).then(function(dbPosition) {
+      res.json(dbPosition);
+    });
+  });
+
 };
+
